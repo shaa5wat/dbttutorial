1549bb10f56f6cb8ff737377f67e15d7bcef63de
Shaaswat Tiwari <shaaswat.tiwari@hashmapinc.com>
2022-07-06 13:39:40 +0000

    source function

--- models/stage/orders.sql
+++ models/stage/orders.sql
@@ -1,6 +1,7 @@
 {{ config(
     schema= 'STAGE',
-    database= 'PC_MATILLION_DB' 
+    database= 'PC_MATILLION_DB', 
+    materialized= 'view'
 )}}
 
 SELECT * FROM "PC_MATILLION_DB"."RAW"."ORDERS"
